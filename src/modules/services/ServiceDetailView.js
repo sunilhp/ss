9ed4e6bc466c7c685a3a525d02c4f5e6aa41b1b1
Ramlal <ramlal@webhopers.in>
2019-06-20 12:21:51 +0530

    push check

--- src/modules/services/ServiceDetailView.js
+++ src/modules/services/ServiceDetailView.js
@@ -141,7 +141,7 @@ const formattedimages = [];
                   'New',
                   'In Progress',
                   'Pending',
-                  'Complete'
+                  'Completed'
                  
                 ]}
                 onSelect={index =>
